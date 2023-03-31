7a2f21ee77dd6dfc12e0751af519079febfa16f5
SnehKalariya <snehkalariya9583@gmail.com>
2023-03-31 12:18:16 +0530

    Initial Commit

---
+++ SqliteDay1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SqliteDay1
+//
+//  Created by Sneh kalariya on 31/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
